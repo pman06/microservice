f47e42a9916cbc01372c6dcf2f7732afa4b1f20f
Paul <freemeonline@gmail.com>
2025-12-23 23:53:13 +0100

    Added logout test to tests

--- account-service/tests/test_refresh_token.py
+++ account-service/tests/test_refresh_token.py
@@ -1,3 +1,4 @@
+from app.utils.password_hash import hash_password
 def login(client):
     res = client.post("/auth/login", json={
         "email": "user@test.com",
@@ -19,3 +20,19 @@ def test_refresh_token_rotation(client, user):
     assert "access_token" in data
     assert "refresh_token" in data
     assert data["refresh_token"] != refresh
+
+def test_refresh_with_revoked_token(client, user):
+    tokens = login(client)
+    refresh = tokens.get_json()["refresh_token"]
+
+    client.post("/auth/logout", json={
+        "refresh_token": refresh
+    })
+
+    res = client.post("/auth/refresh", json={
+        "refresh_token": refresh
+    }) 
+
+    data = res.get_json()
+    assert res.status_code == 401
+    assert data["error"] == "Invalid or expired refresh token"
